f5b67134048741e6263ba4fb73ce2ef4bb4b5343
stvnso <stevensowa2910@gmail.com>
2022-08-16 22:14:57 +0200

    time changes

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+setInterval(refreshTime, 1000);
+
+
 function refreshTime() {
-    const timeDisplay = document.getElementById("time")
-    const dateDisplay = document.getElementById("date")
     const currentDate = new Date()
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-    var currentTime = currentDate.toLocaleTimeString('en-US', { hour12: false })
-    timeDisplay.textContent = currentTime
-    dateDisplay.textContent = currentDate.toLocaleDateString('de-DE', options)
-
+    document.getElementById("time").innerHTML = currentDate.toLocaleTimeString('en-US', { hour12: false })
+    document.getElementById("date").innerHTML = currentDate.toLocaleDateString('de-DE', options)
 
   }
-    setInterval(refreshTime, 1000)
+  
+function startStopWatch(){
+  const currentTime = new Date()
+  document.getElementById("punch-in-time").innerHTML = currentTime.toLocaleTimeString('en-US', { hour12: false })
+}
+
+function test(){
+  const timeWWW = document.getElementById("punch-in-time").innerText
+  alert(timeWWW)
+  document.getElementById("testput").innerHTML = timeWWW
+
+}
+
