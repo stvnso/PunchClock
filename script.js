6fa3978f122ca159bd280c1d4f93b48e4b65d6c9
stvnso <stevensowa2910@gmail.com>
2022-08-17 19:17:07 +0200

    time layout

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ setInterval(refreshTime, 1000);
 
 function refreshTime() {
     const currentDate = new Date()
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const options = {year: 'numeric', month: 'long', day: 'numeric' };
 
     document.getElementById("time").innerHTML = currentDate.toLocaleTimeString('en-US', { hour12: false })
     document.getElementById("date").innerHTML = currentDate.toLocaleDateString('de-DE', options)
@@ -12,13 +12,11 @@ function refreshTime() {
   
 function startStopWatch(){
   const currentTime = new Date()
-  document.getElementById("punch-in-time").innerHTML = currentTime.toLocaleTimeString('en-US', { hour12: false })
+  document.getElementById("punch-in-time").innerHTML = currentTime.toLocaleTimeString('en-US', { hour12: false, hour: '2-digit', minute:'2-digit' })
 }
 
 function test(){
-  const timeWWW = document.getElementById("punch-in-time").innerText
-  alert(timeWWW)
-  document.getElementById("testput").innerHTML = timeWWW
+  const currentTime = new Date()
+  document.getElementById("hours-worked").innerHTML = (document.getElementById("punch-in-time").innerHTML) - 3
 
 }
-
